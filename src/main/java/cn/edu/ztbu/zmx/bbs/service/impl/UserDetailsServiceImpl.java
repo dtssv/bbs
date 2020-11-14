2cf265862f0c4b14610909bb9bdaad1b9e517333
dtssv <709423445@qq.com>
2020-11-14 21:24:22 +0800

    repository

--- src/main/java/cn/edu/ztbu/zmx/bbs/service/impl/UserDetailsServiceImpl.java
+++ src/main/java/cn/edu/ztbu/zmx/bbs/service/impl/UserDetailsServiceImpl.java
@@ -1,12 +1,15 @@
 package cn.edu.ztbu.zmx.bbs.service.impl;
 
 
+import cn.edu.ztbu.zmx.bbs.domain.User;
 import cn.edu.ztbu.zmx.bbs.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 /**
  * @program bbs.UserDetailService
@@ -20,6 +23,8 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     private UserRepository repository;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return repository.findUserByUserNameOrPhoneOrEmail(username,username,username);
+        Optional<User> user = repository.findOne((r,q,c)->c.or(c.equal(r.get("userName"),username),
+                c.equal(r.get("phone"),username),c.equal(r.get("email"),username)));
+        return user.isPresent() ? user.get() : null;
     }
 }
